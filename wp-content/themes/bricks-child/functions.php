66cf61b01941d817b0a768c273eaedafe029123b
Nimixx <tadeasthelen@proton.me>
2024-12-31 20:10:48 +0100

    Add register varaibles

--- wp-content/themes/bricks-child/functions.php
+++ wp-content/themes/bricks-child/functions.php
@@ -5,3 +5,9 @@
 require_once __DIR__ . '/backend/assets/includes/AssetManager.php';
 BricksChild\AssetManager::getInstance();
 
+// Initialize CodeStrict
+require_once __DIR__ . '/backend/assets/includes/CodeStrict.php';
+
+// Initialize Variables Registration
+require_once __DIR__ . '/backend/assets/includes/RegisterVariables.php';
+
